0c36f24fd6a73a8ad02626f02223313f5ce6db6d
tech-interface <sanketpatilcs@gmail.com>
2022-12-31 18:21:20 +0530

    Spring Boot React Application

---
+++ spring-boot-react/src/main/java/com/techinterface/SpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.techinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactApplication.class, args);
+	}
+
+}
